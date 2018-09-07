e09528ca07fd7d5f4d976bcf4b0b31027d089f85
Duane Reinhardt <dwreinhardt75@outlook.com>
2018-09-06 21:03:38 -0400

    add hello form in main.py

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from flask import Flask # imports Flask class from flask module.
+from flask import Flask, request
 
-#app will be the object created by the constructor Flask. 
-# __name__ is a variable controlled by Python that tells code what module it's in
-app = Flask(__name__) 
-
-#the DEBUG configuration setting for the Flask application will be enabled.
-# This enables some behaviors that are helpful when developing Flask apps, 
-# such as displaying errors in the browser, and ensuring file changes are 
-# reloaded while the server is running (aka "host swapping")
+app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")#decorator creates a mapping between the path 
-               # - in this case the root, or "/", and the function 
-               # that we're about to define
+form = """
+<!doctype html>
+<html>
+    <body>
+        <form action="/hello" method="post">
+            <label for="first name">First Name:</label>
+            <input id="first name" type="text" name="first_name" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+@app.route("/")
+def index():
+    return form
 
-def index():#a function of zero variables
-    return "Hello World"#function returns a string literal
+@app.route("/hello", methods=['post'])
 
-#Pass control to the Flask object. 
-# The run function loops forever and never returns, so put it last. 
-# It carries out the responsibilities of a web server, listening for requests
-# and sending responses over a network connection
+def hello():
+    first_name = request.form['first_name']
+    return "<h1> Hello, " + first_name + "</h1>"
+
 app.run()
